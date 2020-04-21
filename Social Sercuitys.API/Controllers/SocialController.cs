6a16435e12ca8bb80d314d8e6bdd555bc13fc96a
1720934884@qq.com <1720934884@qq.com>
2020-04-21 16:01:20 +0800

    公司信息显示

--- Social Sercuitys.API/Controllers/SocialController.cs
+++ Social Sercuitys.API/Controllers/SocialController.cs
@@ -11,10 +11,25 @@ namespace Social_Sercuitys.API.Controllers
     public class SocialController : ApiController
     {
         socialBll bll = new socialBll();
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="pwd"></param>
+        /// <returns></returns>
         [HttpPost]
         public int Login(string name, string pwd)
         {
             return bll.Login(name, pwd);
+        }
+        /// <summary>
+        /// 显示公司注册信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<Company> companies()
+        {
+            return bll.companies();
         }
     }
 }
--- Social.BLL/socialBll.cs
+++ Social.BLL/socialBll.cs
@@ -10,9 +10,23 @@ namespace Social.BLL
     public class socialBll
     {
         socialDal dal = new socialDal();
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="pwd"></param>
+        /// <returns></returns>
         public int Login(string name, string pwd)
         {
             return dal.Login(name, pwd);
+        }
+        /// <summary>
+        /// 显示注册公司信息
+        /// </summary>
+        /// <returns></returns>
+        public List<Company> companies()
+        {
+            return dal.companies();
         }
     }
 }
