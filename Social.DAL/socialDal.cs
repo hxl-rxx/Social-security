e42e7c4d2016da286fbb8abd3b491ea3731bca02
1720934884@qq.com <1720934884@qq.com>
2020-04-21 15:54:05 +0800

    公司信息接口

--- Social.DAL/socialDal.cs
+++ Social.DAL/socialDal.cs
@@ -13,6 +13,12 @@ namespace Social.DAL
     {
         static readonly string Coon = ConfigurationManager.ConnectionStrings["MySql"].ConnectionString;
 
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="pwd"></param>
+        /// <returns></returns>
         public int Login(string name,string pwd)
         {
             using(MySqlConnection connection=new MySqlConnection(Coon))
@@ -20,6 +26,19 @@ namespace Social.DAL
                 string sql = $"select * from userLogin where Name={name} && passWord={pwd}";
                 var query = connection.Query(sql);
                 return Convert.ToInt32(query);
+            }
+        }
+        /// <summary>
+        /// 显示注册公司信息
+        /// </summary>
+        /// <returns></returns>
+        public List<Company> companies()
+        {
+            using (MySqlConnection connection = new MySqlConnection(Coon))
+            {
+                string sql = "select * from company; ";
+                var query = connection.Query<Company>(sql);
+                return query.ToList();
             }
         }
     }
--- Social.Model/userLogin.cs
+++ Social.Model/userLogin.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Social.Model
 {
-    public class Model
+    public class userLogin
     {
         public int ID { get; set; }
         public string Name { get; set; }
